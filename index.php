65f8359601a5bed4570a9322becc1423e15aaaa8
alhajaconhistoria <91439520+alhajaconhistoria@users.noreply.github.com>
2021-09-26 19:25:31 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Alhaja con Historia</title>
+    <link href="style.CSS" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body> 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@300&family=Cormorant+Garamond:ital@1&family=Josefin+Slab:ital@1&family=Tajawal:wght@300&display=swap" rel="stylesheet">
+
+  <header>
+    <nav><li><a class="Logo" href="index.php">Logo</a></li></nav>
+    <ul class="menu">
+      <li><a href="index.php">Inicio</a></li>
+      <li><a href="Servicios.php">Servicios</a></li>
+      <li><a href="Blog.php">Blog</a></li>
+      <li><a href="g">Galería</a></li>
+    </ul>
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+  </header>
+
+    <section class="header">
+      <p>Alhaja con Historia</p>
+      <img src="JPG/AlhajaBanner.jpg" alt="jpg" style="float:left; position: relative;">
+    </section><br>
+    <br>
+    <br>
+    <br><section class ="Título">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@300&family=Cormorant+Garamond:ital@1&family=Josefin+Slab:ital@1&display=swap" rel="stylesheet">
+      <h1>HOLA, SOY WENDY</h1><br>
+      <p>Es innegable que el uso de la propaganda, mediante la creación de simbolismos con los que uno se pueda identificar en un plano emocional, ha mostrado ser una herramienta efectiva para encauzar la insatisfacción popular y erigirte como una figura de cambio. Sutilmente, la propaganda tiene una influencia en nuestra percepción de la realidad. Por eso mismo, este ensayo pretende comparar y contrastar la importancia de la propaganda en el surgimiento de dos estados autoritarios en el continente europeo: el régimen fascista de Benito Mussolini y el régimen nazi de Adolf Hitler.</p>
+      <br>
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
+    <section class="Luego">
+    <p>¿Quieres programar una cita?</p>
+    </section>
+    <form class="contact-form" action="contactform.php" method="POST">
+      <input type="text" name="nombre" placeholder="Mi nombre">
+      <input type="text" name="apellido" placeholder="Mi apellido">
+      <input type="text" name="correo" placeholder="Mi correo electrónico es...">
+      <input type="text" name="celular" placeholder="Mi número celular es...">
+      <input type="text" name="servicio" placeholder="¿Qué servicio deseas?">
+      <button type="submit" name="submit">Enviar</button>
+    </form>
+
+
+    <footer>
+        2020 - Alhajaconhistoria
+    </footer>
+    <script src="Untitled-1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
