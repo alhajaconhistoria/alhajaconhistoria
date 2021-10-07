43ed679e799740c52f590f362277f3d892957a69
alhajaconhistoria <91439520+alhajaconhistoria@users.noreply.github.com>
2021-10-07 04:48:36 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Alhaja con Historia</title>
+    <link href="style.CSS" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body> 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@300&family=Cormorant+Garamond:ital@1&family=Josefin+Slab:ital@1&family=Tajawal:wght@300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&family=Charm&family=La+Belle+Aurore&family=Petit+Formal+Script&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+
+  <header>
+    <nav><li><a src="PNG/logo.png" href="index.html"></a></li></nav>
+    <ul class="menu">
+      <li><a href="index.php">Inicio</a></li>
+      <li><a href="Servicios.php">Servicios</a></li>
+      <li><a href="Blog.php">Blog</a></li>
+      <li><a href="Galería.php">Galería</a></li>
+    </ul>
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+  </header>
+
+    <section class="header">
+      <p>Alhaja con Historia</p>
+      <img src="PNG/wendyalhaja.png" alt="jpg" style="float:left; position: relative;">
+    </section><br>
+    <br>
+    <br>
+    <br><section class ="Título">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@300&family=Cormorant+Garamond:ital@1&family=Josefin+Slab:ital@1&display=swap" rel="stylesheet">
+      <h1>Hola, soy Wendy</h1><br>
+      <p>Desde muy pequeña me gustaba mucho todo lo relacionado a la imagen, de niña mi juego preferido eran las muñecas recortables (y todas las combinaciones interminables que me inventaba). En la adolescencia siempre arreglaba a mis amigas cuando tenían un evento importante, recuerdo verlas irse más hermosas de lo que ya eran y yo sentirme satisfecha de haberlas podido ayudar.</p>
+      <p>Pasaron los años y estudié Educación: amo en la docencia la idea de formar personas y hacerlas mejores, es algo que me inspira. Me casé con Luchito y formé mi familia; fui muchos años mamá a tiempo completo, años hermosos que ví crecer a mis chicos que ahora son grandes. En esos años me enteré de la carrera de Asesoría de Imagen, y me dije: "Allá voy, esto es lo mío". Me las organizé en casa y aquí estoy, enamorada de a lo que me dedico. Me especializé en la Escuela de Imagen Integral LPS, luego estudié Coaching de Iamgen Internacional avalado por la International Association of Coaching de Estados Unidos con la Escuela de Diseño y Moda de Lima. Tengo junto a esto estudios en Coaching de Estilo y Personal Shopper en la Escuela de Unidad Editorial de España. He hecho una especialización en Sostenibilidad con The Fashion Future: The Sustainable Development Goals for Fashion Revolution. Estar en aprendizaje constante me motiva, y soy feliz de ponerlo a tu disposición para poderte ayudar en lo que necesites.</p>
+      <br>
+      <hr class="hy">
+    </section>
+    <br><br><br>
+    <section class="Banner">
+      <section class="blanco">
+       <br>
+       <h1>¿Por qué Alhaja con Historia?</h1>
+       <p>Alhaja es una palabra que me transporta a mi niñez. Siempre, al visitar a mi abuela, ella nos extendía sus brazos y decía: "Ya llegaron mis alhajas". Esa corta frase me hacía sentir especial, valiosa, única, me encantaba sentirme así. Ya con el tiempo descubrí su verdadero significado, y me ha acompañado en todas las etapas de mi vida, ahora aún más con la asesoría de imagen. Decidí incorporar aquí 'Alhaja' porque tiene una conexión directa con la exterioridad e interioridad de las mujeres que busco ayudar, por un lado a repotenciar su imagen, sacarse partido de todas las ventajas que ya tienen, ayudarlas a brillar reflejando su identidad.</p>
+      </section>
+       <img src="PNG/instagram12.png" alt="jpg" style="position: relative;">
+    </section>
+    <br><br><br>
+    <hr class="h">
+    <br>
+    <section class="Luego">
+    <h1>¿Quieres programar una cita?</h1>
+    </section><br>
+    <form class="contact-form" action="contactform.php" method="POST">
+      <input type="text" name="nombre" placeholder="Mi nombre">
+      <input type="text" name="apellido" placeholder="Mi apellido">
+      <input type="text" name="correo" placeholder="Mi correo electrónico es...">
+      <input type="text" name="celular" placeholder="Mi número celular es...">
+      <input type="text" name="servicio" placeholder="¿Qué servicio deseas?">
+      <button type="submit" name="submit">Enviar</button>
+    </form>
+    <br>
+    <br>
+    <br>
+    <br>
+    <footer>
+         <div class="Contactooficial">
+         <txt class="Contacto">Contáctame</txt>
+         <txt class="contacto1">Teléfono: +51 993455631</txt>
+         <txt class="contacto2">Correo: wendym.alhajaconhistoria@gmail.com</txt>
+        </div>
+        <div class="footer-widget">Páginas</div>
+          <ul>
+            <a class="footer1" href="Servicios.php">Servicios</a>
+            <a class="footer2" href="Blog.php">Blog</a>
+            <a class="footer3" href="Galería.php">Galería</a>
+          </ul>
+        <br>
+        <p class="derechos">2021 ALHAJACONHISTORIA | Todos los derechos reservados</p>
+    
+          </div> <!-- end .fwidget --></div>
+    </footer>
+    <script src="Untitled-1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
