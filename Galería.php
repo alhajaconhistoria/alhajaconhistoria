46caa1dfbc60b836b107cd1abbf25d91dbae9ea2
alhajaconhistoria <91439520+alhajaconhistoria@users.noreply.github.com>
2021-10-07 04:01:12 -0500

    Add files via upload

---
+++ Galería.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Alhaja con Historia</title>
+    <link href="stylegaleria.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body> 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@300&family=Cormorant+Garamond:ital@1&family=Josefin+Slab:ital@1&family=Tajawal:wght@300&display=swap" rel="stylesheet">
+
+  <header>
+    <nav><li><a class="Logo" href="index.php">Logo</a></li></nav>
+    <ul class="menu">
+      <li><a href="index.php">Inicio</a></li>
+      <li><a href="Servicios.php">Servicios</a></li>
+      <li><a href="Blog.php">Blog</a></li>
+      <li><a href="Galería.php">Galeria</a></li>
+    </ul>
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+  </header>
+
+      <br><br>
+
+    <section class="Important">
+        <h1>Galería</h1><br>  
+      <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+          <div class="elfsight-app-42ec82ae-2614-4b64-a70f-b9461566da48"></div> 
+    </section>
+    
+    <br><br>
+    <footer>
+        <div class="Contactooficial">
+        <txt class="Contacto">Contactame</txt>
+        <txt class="contacto1">Teléfono: +51 993455631</txt>
+        <txt class="contacto2">Correo: wendym.alhajaconhistoria@gmail.com</txt>
+       </div>
+       <div class="footer-widget">Páginas</div>
+         <ul>
+           <li class="footer1"><a href="Servicios.php">Servicios</a></li>
+           <li class="footer2"><a href="Blog.php">Blog</a></li>
+           <li class="footer3"><a href="Galería.php">Galería</a></li>
+         </ul>
+       <br>
+       <p class="derechos">2021 ALHAJACONHISTORIA | Todos los derechos reservados</p>
+    
+         </div> <!-- end .fwidget --></div>
+    </footer> 
+
+    <script src="Untitled-1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ thankyou.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Alhaja con Historia - ¡Gracias!</title>
+    <link href="stylethankyou.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@300&family=Allura&family=Arima+Madurai:wght@300&family=Charmonman&family=Cormorant+Garamond:ital,wght@1,300&family=Crimson+Text:ital@1&family=Dancing+Script&family=Duru+Sans&family=Forum&family=Josefin+Slab:wght@300&family=Mate&family=Merienda&family=MonteCarlo&family=Nanum+Myeongjo&family=Nixie+One&family=Oranienbaum&display=swap" rel="stylesheet">
+  
+  <section class="Uno">
+      <h1>.     .     .     .     .     .     .     .     .     .     .     .</h1>
+  </section>
+  <section class="Thank-you">
+    <h1>¡Gracias por contactarme!</h1>
+    <br>
+    <br>
+    <p>Te responderé a la brevedad, ¡Ten un lindo día!</p>
+    <br>
+    <li><a class="btn btn-secondary btn-lg" href="index.html">Volver a Inicio</a></li>
+  </section>
+  <section class="Dos">
+      <h1>.  .  .  .  .  .  .  .  .  .  .  .</h1>    
+  </section>
+
+</body>
+
+</html>
+
